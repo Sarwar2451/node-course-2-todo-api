923cfbd32fc2abf69d7e47722648aadac63705d7
unknown <mahrukipinky@yahoo.com>
2018-12-31 18:13:35 +0600

    Add tests for GET /users/me and POST /users

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-require('./config/config.js');
+require('./config/config');
 
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -97,23 +97,24 @@ app.patch('/todos/:id', (req, res) => {
     })
 });
 
+// POST /users
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
   user.save().then(() => {
-      return user.generateAuthToken();
+    return user.generateAuthToken();
   }).then((token) => {
-      res.header('x-auth', token).send(user);
+    res.header('x-auth', token).send(user);
   }).catch((e) => {
     res.status(400).send(e);
   })
 });
 
-
 app.get('/users/me', authenticate, (req, res) => {
-    res.send(req.user);
+  res.send(req.user);
 });
+
 
 app.listen(port, () => {
     console.log(`Started up at port ${port}`);
